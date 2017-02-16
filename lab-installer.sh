9691ccbe782a9289cfd0f97c7b72e686a7cd517d
Camilo Martinez <cmart@decolector.net>
2017-02-16 16:41:34 -0500

    delete user variable, comments

--- lab-installer.sh
+++ lab-installer.sh
@@ -6,10 +6,13 @@
 
 
 # homebrew
-USR = ${USERNAME}
+
 yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# git wget mc
 brew install git wget mc
+
+#instala pip, virtualenv and virtualenvwrapper como superusuario
 sudo -s <<EOF
 
 sudo -H easy_install pip
@@ -18,9 +21,10 @@ sudo -H pip install virtualenv
 
 sudo -H pip install virtualenvwrapper --ignore-installed six
 EOF
-
+#node version manager
 brew install nvm
 
+#configura variables de entorno en .basrc y en la shell actual
 touch $HOME/.bashrc
 echo "export WORKON_HOME=~/.envs" >> $HOME/.bashrc
 echo ". /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
@@ -32,13 +36,16 @@ export NVM_DIR=~/.nvm
 export WORKON_HOME=~/.envs
 . /usr/local/opt/nvm/nvm.sh
 
+#instala la version estable de nodejs
 nvm install node
 
+#gulp grunt bower
 npm install -g gulp-cli grunt-cli bower
 
-
+#pandoc
 brew install pandoc
 
+#jekyll como sudo
 sudo -s <<EOF
 gem install jekyll
 
