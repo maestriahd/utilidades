514ec76950e706da62f73e7721b49aad8da66547
Camilo Martinez <cmart@decolector.net>
2017-02-16 15:20:40 -0500

    changes to script

--- lab-installer.sh
+++ lab-installer.sh
@@ -7,7 +7,7 @@
 
 # homebrew
 USR = ${USERNAME}
-sudo -u ${USERNAME} yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo -u $USR yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 sudo -H easy_install pip
 
@@ -15,19 +15,19 @@ sudo -H pip install virtualenv
 
 sudo -H pip install virtualenvwrapper --ignore-installed six
 
-sudo -u ${USERNAME} brew install nvm
+sudo -u $USR brew install nvm
 
-sudo -u ${USERNAME} touch $HOME/.bashrc
-sudo -u ${USERNAME} echo "export WORKON_HOME=~/.envs" >> $HOME/.bashrc
-sudo -u ${USERNAME} echo ". /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
-sudo -u ${USERNAME} echo "export NVM_DIR=$HOME/.nvm" >> $HOME/.bashrc
-sudo -u ${USERNAME} echo ". /usr/local/opt/nvm/nvm.sh" >> $HOME/.bashrc
+sudo -u $USR touch $HOME/.bashrc
+sudo -u $USR echo "export WORKON_HOME=~/.envs" >> $HOME/.bashrc
+sudo -u $USR echo ". /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
+sudo -u $USR echo "export NVM_DIR=$HOME/.nvm" >> $HOME/.bashrc
+sudo -u $USR echo ". /usr/local/opt/nvm/nvm.sh" >> $HOME/.bashrc
 
-sudo -u ${USERNAME} . $HOME/.bashrc
+sudo -u $USR . $HOME/.bashrc
 
-sudo -u ${USERNAME} nvm install -g node
+sudo -u $USR nvm install -g node
 
-sudo -u ${USERNAME} npm install -g gulp-cli grunt-cli bower
+sudo -u $USR npm install -g gulp-cli grunt-cli bower
 
 gem install jekyll
 
